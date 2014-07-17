caa830a0c8aa0b50c2c395e0e570ff65b6eda477
Rui Onodera <deraru@gmail.com>
2014-07-18 01:03:35 +0900

    improve products page

--- app/scripts/services/product.js
+++ app/scripts/services/product.js
@@ -11,9 +11,63 @@ angular.module('jamesApp')
   .service('Product', function Product() {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    var kanbanSan = { id: 'kanban-san', name: 'Kanban-San', description: '旅が好きなすべての人に全く新しい旅行体験を提供します。' };
-    var kanbanPlatform = { id: 'kanban-platform', name: 'Kanban Platform', description: '電子看板を誰でも手軽に作れるプラットフォームを提供します。' };
-    var pawasupo = { id: 'pawasupo', name: 'Pawasupo', description: '新しいパワースポット巡りを提供します。' };
+    var kanbanSan = {
+      id: 'kanban-san',
+      name: 'Kanban-San',
+      catchPhrase: '大切なお客様に全く新しい旅行体験を',
+      descriptions: [
+        {
+          title: '大切なお客様に全く新しい旅行体験を',
+          lead: '旅が好きなすべての人に全く新しい旅行体験を提供します。'
+        },
+        {
+          title: '使いやすい管理画面で今の情報を',
+          lead: 'お客様に提供する情報はいつでもどこからでも管理できます。'
+        },
+        {
+          title: '最新技術と洗練されたシステムを',
+          lead: '専用端末をお客様にお配りする必要はもうありません。'
+        }
+      ]
+    };
+    var kanbanPlatform = {
+      id: 'kanban-platform',
+      name: 'Kanban Platform',
+      catchPhrase: '手軽にiBeaconを使った電子看板を',
+      descriptions: [
+        {
+          title: '手軽にiBeaconを使った電子看板を',
+          lead: 'スマホ・ウェアラブルデバイス時代の電子看板プラットフォームです。'
+        },
+        {
+          title: 'アプリケーションの開発に専念しましょう',
+          lead: '面倒な管理画面開発、サーバー管理をする必要はありません。'
+        },
+        {
+          title: '高度で効果的なデータ分析を',
+          lead: '集まったビッグデータの分析・レポーティング環境を提供します。'
+        }
+      ]
+    };
+    var pawasupo = {
+      id: 'pawasupo',
+      name: 'Pawasupo',
+      catchPhrase: 'パワースポット巡りの新しいアプリケーション',
+      descriptions: [
+        {
+          title: 'パワースポット巡りの新しいアプリケーション',
+          lead: 'ここだけのストーリー、お教えします。'
+        },
+        {
+          title: 'パワースポットをコレクション',
+          lead: '全国のパワースポットをコンプリートしよう。'
+        },
+        {
+          title: 'ストーリーテラーがあなたの手元に',
+          lead: 'スマホを片手にパワースポットを巡りましょう。'
+        }
+      ]
+    };
     var products = [
       kanbanSan,
       kanbanPlatform,
