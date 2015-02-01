6fd0bd40d343018ec4f2555a6e61801490f5e6f4
Rui Onodera <deraru@gmail.com>
2015-02-02 00:50:22 +0900

    remove bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -366,11 +366,6 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
-        }, {
-          expand: true,
-          cwd: '.',
-          src: 'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*',
-          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
